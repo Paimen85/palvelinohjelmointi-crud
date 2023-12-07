f406eb8734ac69f12849b95bff507748813ddcb8
Paimen85 <evgeni.baranov@gmail.com>
2023-12-08 01:40:13 +0200

    event entity class created

---
+++ src/main/java/com/example/crud/crudreact/model/Event.java
@@ -0,0 +1,35 @@
+package com.example.crud.crudreact.model;
+
+import java.sql.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Entity
+@Table(name = "events")
+@Data
+public class Event {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "event_title")
+	private String eventTitle;
+
+	@Column(name = "event_description")
+	private String EventDescription;
+
+	@Column(name = "event_date")
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private Date eventDate;
+
+	private String category;
+}
