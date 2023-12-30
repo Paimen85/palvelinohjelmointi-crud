c5988fe6ddd701aebf3f41800fc72b3347f94add
Paimen85 <evgeni.baranov@gmail.com>
2023-12-30 15:27:10 +0200

    CustomUserDetailsService is created

--- src/main/java/com/example/crud/crudreact/repository/UserRepository.java
+++ src/main/java/com/example/crud/crudreact/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.example.crud.crudreact.repository;
 import com.example.crud.crudreact.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
+    Optional<User> findByUsernameOrEmail(String username, String email);
 }
